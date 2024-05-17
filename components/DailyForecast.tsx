c88192879dbcfe869619b3609bf6843928821894
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-05-17 13:20:12 +0300

    adding logic to DailyForecast, now it is based on real data

--- components/DailyForecast.tsx
+++ components/DailyForecast.tsx
@@ -1,9 +1,15 @@
 import { CalendarDaysIcon } from 'react-native-heroicons/outline';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import { Weather } from '../types/weatherType';
 
 import WeatherForecastPreview from './WeatherForecastPreview';
 
-export default function DailyForecast() {
+type DailyForecastProps = {
+	currWeather: Weather;
+};
+
+export default function DailyForecast({ currWeather }: DailyForecastProps) {
 	return (
 		<View style={styles.dailyForecastContainer}>
 			<View style={styles.headlineContainer}>
@@ -11,21 +17,29 @@ export default function DailyForecast() {
 				<Text style={styles.headlineText}>Daily forecast</Text>
 			</View>
 
-			<ScrollView horizontal={true} showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 15 }}>
-				<WeatherForecastPreview />
-				<WeatherForecastPreview />
-				<WeatherForecastPreview />
-				<WeatherForecastPreview />
-				<WeatherForecastPreview />
-				<WeatherForecastPreview />
-				<WeatherForecastPreview />
-			</ScrollView>
+			<FlatList
+				style={styles.weatherForecastList}
+				data={currWeather?.forecast}
+				renderItem={({ item }) => {
+					return <WeatherForecastPreview forecast={item} />;
+				}}
+				horizontal
+				keyExtractor={(item, index) => `${item.text}-${index}`}
+				ItemSeparatorComponent={() => <View style={{ width: 1 }}></View>}
+				ListEmptyComponent={() => <Text>No Items Found</Text>}
+			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	dailyForecastContainer: {},
+	dailyForecastContainer: {
+		flex: 1,
+	},
+
+	weatherForecastList: {
+		flexDirection: 'row',
+	},
 
 	headlineContainer: {
 		flexDirection: 'row',
