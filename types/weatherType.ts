469f36c4a0322039d41aa7f382c203dd1b195dbe
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-05-17 13:26:44 +0300

    creating file of types

---
+++ types/weatherType.ts
@@ -0,0 +1,45 @@
+export type Weather = {
+	location: {
+		country: string;
+		name: string;
+		localtime: string;
+	};
+	condition: {
+		text: string;
+		icon: string;
+	};
+
+	temp: number;
+	humidity: number;
+	wind: number;
+
+	forecast: Forecast[];
+};
+
+export type Forecast = {
+	day: number;
+	temp: number;
+	text: string;
+};
+
+export type Coordinates = {
+	latitude: number;
+	longitude: number;
+};
+
+export type WeatherMap = {
+	[day: number]: Weather[];
+};
+
+export type FilterBy = {
+	txt: string;
+	country?: string;
+};
+
+export type Location = {
+	country: string;
+	city: string;
+	lat: number;
+	lon: number;
+	_id: number;
+};
