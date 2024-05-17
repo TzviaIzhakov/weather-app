b44959bd1d3e04bdd0c1691b8b153989e6e2a6e0
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-05-17 13:07:55 +0300

    using the weather hook in the context in order to get states globally

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
-import { ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
+
+import WeatherContext from './contexts/WeatherContext';
+
+import useWeather from './hooks/useWeather';
 
 import Home from './screens/Home';
 
 import backgroundImg from './assets/images/bg.png';
 
 export default function App() {
+	const weatherHandler = useWeather();
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<ImageBackground style={styles.backgroundImag} source={backgroundImg} blurRadius={55} />
-			<Home />
+			<WeatherContext.Provider value={weatherHandler}>
+				<Home />
+			</WeatherContext.Provider>
 		</SafeAreaView>
 	);
 }
