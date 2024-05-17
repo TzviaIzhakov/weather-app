0329589d2f51d16d52a07a163cfcb69047b22936
TzviaIzhakov <tzvia.izhakov@gmail.com>
2024-05-17 15:51:00 +0300

    adding documentation

--- services/weatherService.ts
+++ services/weatherService.ts
@@ -13,11 +13,13 @@ export const weatherService = {
 
 const STORAGE_KEY = 'WEATHERDB';
 
-function getForecast(forecastday: any) {
+//getForecast returns a formatted array of daily forecast weather
+function getForecast(forecastday: any): Forecast[] {
 	forecastday = forecastday.map((f: Forecast) => ({ day: f.date_epoch, text: f.day.condition.text, temp: f.day.avgtemp_c }));
 	return forecastday;
 }
 
+//getFormattedPayload return a formatted weather
 function getFormattedPayload(weather: any): Weather {
 	const { location, current, forecast } = weather;
 	const { forecastday } = forecast;
@@ -38,6 +40,7 @@ function getFormattedPayload(weather: any): Weather {
 	};
 }
 
+// getDefaultLocation returns the current location of the user using is latitude and longitude
 async function getDefaultLocation(latitude: number, longitude: number): Promise<Weather> {
 	try {
 		const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json}`, {
@@ -57,40 +60,52 @@ async function getDefaultLocation(latitude: number, longitude: number): Promise<
 	}
 }
 
+//query function returns the current weather  from cache or from the api
 async function query(filterBy = { txt: '', country: '' }, latitude: number, longitude: number): Promise<Weather> {
 	try {
-		let fetchedWeather;
+		// dayTimeStamp is the timestamp of the current day when the hour is 12PM
 		const dayTimeStamp = weatherUtils.getCurrentDayTimestamp();
+		// storedWeathers is the stored data in the storage : {[day:number] : Weather[]}
 		let storedWeathers: WeatherMap = (await storageService.query(STORAGE_KEY)) || {};
 
-		if (Object.keys(storedWeathers).length !== 0 && storedWeathers[dayTimeStamp] && filterBy.txt === '') {
-			return storedWeathers[dayTimeStamp][0];
-		} else if (filterBy.txt === '') {
-			storedWeathers[dayTimeStamp] = [];
-			fetchedWeather = await getDefaultLocation(latitude, longitude);
-		} else {
-			const regex1 = new RegExp(filterBy.txt, 'i');
-			const regex2 = new RegExp(filterBy.country, 'i');
-			const existingWeather = storedWeathers[dayTimeStamp].find((w) => regex1.test(w.location.name) && regex2.test(w.location.country));
-
-			if (existingWeather) {
-				return existingWeather;
+		if (filterBy.txt === '') {
+			// If no filter is applied and stored weather data exists for the current day, return the first entry which represents also the location of the user
+			if (storedWeathers[dayTimeStamp]?.length > 0) {
+				return storedWeathers[dayTimeStamp][0];
 			}
 
-			const res = await axios.get(`https://api.weatherapi.com/v1/forecast.json`, {
-				params: {
-					key: process.env.EXPO_PUBLIC_WEATHER_KEY,
-					days: 7,
-					q: filterBy.txt,
-					aqi: 'no',
-				},
-			});
-
-			fetchedWeather = getFormattedPayload(res.data);
+			// If no filter is applied and no stored data exists, fetch default location weather
+			storedWeathers[dayTimeStamp] = [];
+			//fetch the current weather with the current location of the user
+			const fetchedWeather = await getDefaultLocation(latitude, longitude);
+			//pushing the current weather to the storedWeathers[dayTimeStamp] array
+			storedWeathers[dayTimeStamp].push(fetchedWeather);
+			//storing it in the storage
+			await storageService.saveToStorage(STORAGE_KEY, storedWeathers);
+			return fetchedWeather;
 		}
-
+		// If  filter is applied there is an option that the user searched again a city name, this is checked by the country also
+		const regex1 = new RegExp(filterBy.txt, 'i');
+		const regex2 = new RegExp(filterBy.country, 'i');
+		const existingWeather = storedWeathers[dayTimeStamp]?.find((w) => regex1.test(w.location.name) && regex2.test(w.location.country));
+		//returns the existingWeather from storage if the country ia in the storage
+		if (existingWeather) {
+			return existingWeather;
+		}
+		//if the weather doesn't stored in the storage the servuce will call to the api with the location the user searched
+		const res = await axios.get(`https://api.weatherapi.com/v1/forecast.json`, {
+			params: {
+				key: process.env.EXPO_PUBLIC_WEATHER_KEY,
+				days: 7,
+				q: filterBy.txt,
+				aqi: 'no',
+			},
+		});
+		// fetchedWeather is the weather object with specific keys that needed
+		const fetchedWeather = getFormattedPayload(res.data);
+		storedWeathers[dayTimeStamp] = storedWeathers[dayTimeStamp] || [];
 		storedWeathers[dayTimeStamp].push(fetchedWeather);
-
+		//putting the weather in the storge
 		await storageService.saveToStorage(STORAGE_KEY, storedWeathers);
 		return fetchedWeather;
 	} catch (error) {
@@ -98,6 +113,7 @@ async function query(filterBy = { txt: '', country: '' }, latitude: number, long
 	}
 }
 
+//queryLocations function return  the results of the locations that the user searched for, it uses the api endpoint
 async function queryLocations(text: string): Promise<Location[]> {
 	try {
 		const res = await axios.get(`https://api.weatherapi.com/v1/search.json?key=${process.env.EXPO_PUBLIC_WEATHER_KEY}&q=${text}`);
@@ -107,6 +123,7 @@ async function queryLocations(text: string): Promise<Location[]> {
 	}
 }
 
+//this function returns the default filter
 function getDefaultFilter() {
 	return { txt: '' };
 }
